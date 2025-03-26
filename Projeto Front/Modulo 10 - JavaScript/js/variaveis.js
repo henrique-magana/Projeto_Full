132dec785ea342c343d55e4c4fb8aaf636cef565
henrique magana <henriquesmagana@gmail.com>
2025-03-26 11:00:46 -0300

    Voltando ao JS

---
+++ Projeto Front/Modulo 10 - JavaScript/js/variaveis.js
@@ -0,0 +1,14 @@
+var teste = "nome lala"
+var numero = 17
+var numeroFrac = 17.9394
+var boolean = false
+
+
+console.log(typeof(numeroFrac))
+console.log(teste)
+console.log(numero)
+console.log(boolean)
+
+document.write(numero)
+document.write('<br>')
+document.write(boolean)
