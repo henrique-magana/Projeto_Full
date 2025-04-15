a4c67addd69c425dffd16a0c7b7d67bb7c462771
henrique magana <henriquesmagana@gmail.com>
2025-04-15 14:58:58 -0300

    Seja o que Deus quiser

---
+++ React/Modulo 1/hello-world/src/App.jsx
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>E la vamos nos novamente, #pracima</h1>
+
+      <a href="https://www.google.com" class="menu">
+        Google
+      </a>
+    </div>
+  );
+}
+
+export default App;
